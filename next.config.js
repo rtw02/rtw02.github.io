19a308c5a38df6be4eee7e462513e82da866ef7e
ryan-bkln <ryan.wong@bkln.com>
2023-07-31 22:49:28 -0400

    remove starter app elements

--- next.config.js
+++ next.config.js
@@ -2,16 +2,5 @@
 const nextConfig = {}
 
 module.exports = {
-    // other Next.js configurations go here
-
-    // Fix for the first warning
-    experimental: {
-        images: true,
-        // other experimental features go here
-    },
-
-    // Fix for the second warning
     output: "export",
-
-    // other Next.js configurations go here
 };
