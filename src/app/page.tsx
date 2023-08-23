26b13df2225057be56503d9877287083391014d2
rtw02 <rtwong02@gmail.com>
2023-08-23 00:39:14 -0400

    add bootstrap to website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,29 @@
-import Image from 'next/image'
+import Image from 'next/image';
+import Navbar from './_app';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+    <div>
+      <Navbar /> {/* Include the Bootstrap Navbar component here */}
+      
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div className="z-10 w-full max-w-5xl flex items-center justify-between font-mono text-sm lg:flex">
+          <nav className="nav">
+            <a className="nav-link active" aria-current="page" href="#">
+              Active
+            </a>
+            <a className="nav-link" href="#">
+              Link
+            </a>
+            <a className="nav-link" href="#">
+              Link
+            </a>
+          </nav>
+          <p className="m-0 max-w-[300px] text-sm opacity-50">
+            Personal Website
           </p>
-      </div>
-    </main>
-  )
+        </div>
+      </main>
+    </div>
+  );
 }
