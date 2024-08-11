aac512da513b1b647595a6bd4a639cfe6b65aba4
rtw02 <rtwong02@gmail.com>
2024-08-11 16:00:33 -0400

    space deletion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,6 @@ export default function Home() {
       <Navbar />
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <div className="z-10 w-full max-w-5xl flex items-center justify-between font-mono text-sm lg:flex">
-
         </div>
       </main>
     </div>
